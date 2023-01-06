5df3b191d59c8d8391ff22cc20cdca059ca8f42f
ahmedkhaledhasan40@gmail.com <ahmedkhaledhasan40@gmail.com>
2023-01-06 12:33:39 -0800

    created MyApp Statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ChatPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
